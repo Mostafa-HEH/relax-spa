350a6a1229abfefdd8d8aeb96dc0eff931a32895
Mostafa Hassan <mostafa813765@gmial.com>
2022-02-22 10:25:19 +0200

    start about page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./Layouts/Navbar";
 import Home from "./Pages/Home";
+import About from "./Pages/About";
 import Footer from "./Layouts/Footer";
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
       </Routes>
       <Footer />
     </>
