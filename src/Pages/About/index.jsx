17d670638eb7262def5031e5bcb31826f51b6b29
Mostafa Hassan <mostafa813765@gmail.com>
2022-02-22 10:25:19 +0200

    start about page

---
+++ src/Pages/About/index.jsx
@@ -0,0 +1,14 @@
+import Grid from "@mui/material/Grid";
+import { useStyles } from "./styles";
+
+const About = () => {
+  const classes = useStyles();
+
+  return (
+    <Grid item container className={classes.container}>
+      about
+    </Grid>
+  );
+};
+
+export default About;
