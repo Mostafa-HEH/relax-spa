71fe5cf5e906e92cd4691a2c9b40915a3aa595bc
Mostafa Hassan <mostafa813765@gmial.com>
2022-02-26 20:41:36 +0200

    add search section

--- src/Pages/Services/index.jsx
+++ src/Pages/Services/index.jsx
@@ -1,12 +1,14 @@
 import Grid from "@mui/material/Grid";
+
 import { useStyles } from "./styles";
+import SearchSection from "./SearchSection";
 
 const Services = () => {
   const classes = useStyles();
 
   return (
     <Grid container className={classes.container}>
-      services
+      <SearchSection />
     </Grid>
   );
 };
