62080e8e6a68c478fd9f4effb5f17f423824ee03
Mostafa Hassan <mostafa813765@gmial.com>
2022-02-27 20:13:16 +0200

    start connect us page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./Layouts/Navbar";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Services from "./Pages/Services";
+import ConnectUs from "./Pages/ConnectUs";
 import Footer from "./Layouts/Footer";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/about" element={<About />} />
         <Route exact path="/services" element={<Services />} />
+        <Route exact path="/connectus" element={<ConnectUs />} />
       </Routes>
       <Footer />
     </>
---
+++ src/Pages/ConnectUs/styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from "@mui/styles";
+
+export const useStyles = makeStyles((theme) => ({
+  container: {
+    "&.MuiGrid-root": {
+      gap: theme.spacing(8),
+      marginTop: theme.spacing(8),
+    },
+  },
+}));
