ae9eb9d8010665e1fb0b1c26009e0ed0453a813a
Mostafa Hassan <mostafa813765@gmial.com>
2022-02-27 20:54:28 +0200

    add phones ,emails, social links section

--- src/Pages/ConnectUs/index.jsx
+++ src/Pages/ConnectUs/index.jsx
@@ -1,13 +1,23 @@
 import Grid from "@mui/material/Grid";
 
 import { useStyles } from "./styles";
+import SectionsTitleWithLink from "../../Layouts/SectionsTitleWithLink";
+import PhonesEmailsSocial from "./PhonesEmailsSocial";
 
 const ConnectUs = () => {
   const classes = useStyles();
 
   return (
     <Grid item container className={classes.container}>
-      connect us
+      <SectionsTitleWithLink heading="Keep in touch with us" />
+      <Grid item container className={classes.subContainer}>
+        <Grid item container md={6}>
+          <PhonesEmailsSocial />
+        </Grid>
+        <Grid item container md={6}>
+          massage form
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
