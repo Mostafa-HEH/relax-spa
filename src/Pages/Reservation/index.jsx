d6fe0830184441a4815d3f2e3e07960a618ad8c8
Mostafa Hassan <mostafa813765@gmail.com>
2022-02-28 17:02:13 +0200

    add start slide

--- src/Pages/Reservation/index.jsx
+++ src/Pages/Reservation/index.jsx
@@ -1,13 +1,15 @@
+import React from "react";
 import Grid from "@mui/material/Grid";
 
 import { useStyles } from "./styles";
+import StartSlide from "./StartSlide";
 
 const Reservation = () => {
   const classes = useStyles();
 
   return (
     <Grid item container className={classes.container}>
-      reservation
+      <StartSlide />
     </Grid>
   );
 };
--- src/Pages/Reservation/startSlide.styles.js
+++ src/Pages/Reservation/startSlide.styles.js
@@ -3,17 +3,16 @@ import { makeStyles } from "@mui/styles";
 export const useStyles = makeStyles((theme) => ({
   container: {
     "&.MuiGrid-root": {
-      position: "relative",
-      marginTop: theme.spacing(4),
-      marginBottom: theme.spacing(4),
-      maxWidth: "700px",
-      margin: "auto",
-      padding: `${theme.spacing(8)} ${theme.spacing(10)}`,
-      boxShadow: theme.shadows[2],
-      backgroundColor: theme.palette.common.white,
+      flexDirection: "column",
+      gap: theme.spacing(4),
+      justifyContent: "center",
+      textAlign: "center",
+      marginTop: theme.spacing(12),
+      marginBottom: theme.spacing(12),
 
       [theme.breakpoints.down("sm")]: {
-        padding: `${theme.spacing(8)} ${theme.spacing(2)}`,
+        marginTop: theme.spacing(4),
+        marginBottom: theme.spacing(4),
       },
     },
   },
